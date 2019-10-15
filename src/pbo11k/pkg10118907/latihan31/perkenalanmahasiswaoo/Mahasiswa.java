215554ff5bfb724d76f4793dd1f39886f08ebfdc
unknown <fadlullahyazid@gmail.com>
2019-10-15 23:13:09 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan31/perkenalanmahasiswaoo/Mahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan31.perkenalanmahasiswaoo;
+
+/**
+ *
+ * @author programmer
+ */
+public class Mahasiswa {
+    public String nim, nama;
+    
+    public void perkenalkanDiri() {
+        System.out.println("Hello Everyone");
+        System.out.println("My NIM is "+nim);
+        System.out.println("My name is "+nama);
+        System.out.println("");
+    }
+}
---
+++ src/pbo11k/pkg10118907/latihan31/perkenalanmahasiswaoo/PBO11K10118907Latihan31PerkenalanMahasiswaOO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan31.perkenalanmahasiswaoo;
+
+/**
+ *
+ * @author programmer
+ */
+public class PBO11K10118907Latihan31PerkenalanMahasiswaOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mahasiswa m = new Mahasiswa();
+        m.nim = "10110269";
+        m.nama = "Rizki Adam Kurniawan";
+        m.perkenalkanDiri();
+        
+        
+        Mahasiswa m2 = new Mahasiswa();
+        m2.nim = "10110207";
+        m2.nama = "Indra Tiola";
+        m2.perkenalkanDiri();
+        
+        
+        Mahasiswa m3 = new Mahasiswa();
+        m3.nim = "10110271";
+        m3.nama = "Robi Tnzil Ganefi";
+        m3.perkenalkanDiri();
+        
+        
+        Mahasiswa m4 = new Mahasiswa();
+        m4.nim = "10110269";
+        m4.nama = "Muhammad Nur Awaludin";
+        m4.perkenalkanDiri();
+        
+        
+        
+    }
+    
+}
